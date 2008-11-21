85721b595d4fb7fe3b2c20f76d90e35bedc92232
Lars Stoltenow <penma@snickers.penma.de>
2008-11-21 13:32:54 +0100

    test/pour: some other tests and a quick but useless plausibility test.

--- test/pour.lua
+++ test/pour.lua
@@ -1,57 +1,89 @@
 #!/usr/bin/env lua
 require "guckets"
 
-b1 = guckets.bucket:new()
-b2 = guckets.bucket:new()
-b1.water_max = 6
-b2.water_max = 5
-
--- Table storing the expected results.
--- rows: source, cols: target, list entries: values b1/b2 after forward/reverse pour
-expected = {
-	--  to 0            to 1            to 2            to 3            to 4            to 5
-	{ { 0, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 2, 2, 0 }, { 0, 3, 3, 0 }, { 0, 4, 4, 0 }, { 0, 5, 5, 0 } }, -- from 0
-	{ { 0, 1, 1, 0 }, { 0, 2, 2, 0 }, { 0, 3, 3, 0 }, { 0, 4, 4, 0 }, { 0, 5, 5, 0 }, { 1, 5, 6, 0 } }, -- from 1
-	{ { 0, 2, 2, 0 }, { 0, 3, 3, 0 }, { 0, 4, 4, 0 }, { 0, 5, 5, 0 }, { 1, 5, 6, 0 }, { 2, 5, 6, 1 } }, -- from 2
-	{ { 0, 3, 3, 0 }, { 0, 4, 4, 0 }, { 0, 5, 5, 0 }, { 1, 5, 6, 0 }, { 2, 5, 6, 1 }, { 3, 5, 6, 2 } }, -- from 3
-	{ { 0, 4, 4, 0 }, { 0, 5, 5, 0 }, { 1, 5, 6, 0 }, { 2, 5, 6, 1 }, { 3, 5, 6, 2 }, { 4, 5, 6, 3 } }, -- from 4
-	{ { 0, 5, 5, 0 }, { 1, 5, 6, 0 }, { 2, 5, 6, 1 }, { 3, 5, 6, 2 }, { 4, 5, 6, 3 }, { 5, 5, 6, 4 } }, -- from 5
-	{ { 1, 5, 6, 0 }, { 2, 5, 6, 1 }, { 3, 5, 6, 2 }, { 4, 5, 6, 3 }, { 5, 5, 6, 4 }, { 6, 5, 6, 5 } }  -- from 6
-}
-
 pass = 0
 fail = 0
-total = 0
 
-for ba1 = 0, 6 do
-	for ba2 = 0, 5 do
-		b1.water = ba1
-		b2.water = ba2
-		
-		b1:pour_to(b2)
-		
-		if b1.water == expected[ba1+1][ba2+1][1] and b2.water == expected[ba1+1][ba2+1][2] then
-			io.write("O")
-			pass = pass + 1
-		else
-			io.write(".")
-			fail = fail + 1
+-- run a test for everything up to m1 (maximum water bucket 1) and m2
+-- verify using table t, row index: source + 1, col index: target + 1
+-- list entries: expected state b1/b2
+function test_pour_table(m1, m2, t)
+	b1 = guckets.bucket:new()
+	b2 = guckets.bucket:new()
+	b1.water_max = m1
+	b2.water_max = m2
+	
+	for ba1 = 0, m1 do
+		for ba2 = 0, m2 do
+			b1.water = ba1
+			b2.water = ba2
+			
+			b1:pour_to(b2)
+			
+			if b1.water == t[ba1+1][ba2+1][1] and b2.water == t[ba1+1][ba2+1][2] then
+				io.write("O")
+				pass = pass + 1
+			else
+				io.write(".")
+				fail = fail + 1
+			end
 		end
-		
-		b2:pour_to(b1)
-		
-		if b1.water== expected[ba1+1][ba2+1][3] and b2.water == expected[ba1+1][ba2+1][4] then
-			io.write("O")
-			pass = pass + 1
-		else
-			io.write(".")
-			fail = fail + 1
-		end
-		
-		total = total + 2
 	end
 end
+
+-- run a test for everything up to m1 (maximum water bucket 1) and m2
+-- verify by doing a simple check whether nothing overflowed, and whether no
+-- water was leaked (too much or too little water)
+function test_pour_plausible(m1, m2)
+	b1 = guckets.bucket:new()
+	b2 = guckets.bucket:new()
+	b1.water_max = m1
+	b2.water_max = m2
+	
+	for ba1 = 0, m1 do
+		for ba2 = 0, m2 do
+			b1.water = ba1
+			b2.water = ba2
+			
+			b1:pour_to(b2)
+			
+			if   b1.water + b2.water == ba1 + ba2
+			 and b1.water <= m1 and b2.water <= m2
+			 then
+				io.write("O")
+				pass = pass + 1
+			else
+				io.write(".")
+				fail = fail + 1
+			end
+		end
+	end
+end
+
+test_pour_table(6, 5, {
+	--  to 0      to 1      to 2      to 3      to 4      to 5
+	{ { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 } }, -- from 0
+	{ { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 5 } }, -- from 1
+	{ { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 5 }, { 2, 5 } }, -- from 2
+	{ { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 5 }, { 2, 5 }, { 3, 5 } }, -- from 3
+	{ { 0, 4 }, { 0, 5 }, { 1, 5 }, { 2, 5 }, { 3, 5 }, { 4, 5 } }, -- from 4
+	{ { 0, 5 }, { 1, 5 }, { 2, 5 }, { 3, 5 }, { 4, 5 }, { 5, 5 } }, -- from 5
+	{ { 1, 5 }, { 2, 5 }, { 3, 5 }, { 4, 5 }, { 5, 5 }, { 6, 5 } }  -- from 6
+})
+
+test_pour_table(5, 3, {
+	--  to 0      to 1      to 2      to 3
+	{ { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 } }, -- from 0
+	{ { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 3 } }, -- from 1
+	{ { 0, 2 }, { 0, 3 }, { 1, 3 }, { 2, 3 } }, -- from 2
+	{ { 0, 3 }, { 1, 3 }, { 2, 3 }, { 3, 3 } }, -- from 3
+	{ { 1, 3 }, { 2, 3 }, { 3, 3 }, { 4, 3 } }, -- from 4
+	{ { 2, 3 }, { 3, 3 }, { 4, 3 }, { 5, 3 } }  -- from 5
+})
+
+test_pour_plausible(15, 5)
+
 io.write("\n")
 
-io.write("Pass: " .. pass .. " Fail: " .. fail .. " Total: " .. total .. "\n")
+io.write("Pass: " .. pass .. " Fail: " .. fail .. " Total: " .. (pass + fail) .. "\n")
 os.exit(fail)
