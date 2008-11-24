fbe591258484ab0bf01886cc4bf51b8bf0a49cac
Lars Stoltenow <penma@penma.de>
2008-11-24 01:50:59 +0100

    Inserted the new level 3 into the default set

--- data/levels/default.pl
+++ data/levels/default.pl
@@ -6,6 +6,7 @@ push(@{$levelset->{levels}}, "default/level1.pl");
 push(@{$levelset->{levels}}, "default/level2.pl");
 push(@{$levelset->{levels}}, "default/level3.pl");
 push(@{$levelset->{levels}}, "default/level4.pl");
-#push(@{$levelset->{levels}}, "level5.pl");
-push(@{$levelset->{levels}}, "default/level6.pl");
+push(@{$levelset->{levels}}, "default/level5.pl");
+#push(@{$levelset->{levels}}, "default/level6.pl");
+push(@{$levelset->{levels}}, "default/level7.pl");
 
