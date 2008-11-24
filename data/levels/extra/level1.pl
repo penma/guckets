1c7c1d5d1e4072da2ed1d0d0fa7b3b6f0a899d89
Lars Stoltenow <penma@penma.de>
2008-11-24 13:27:12 +0100

    extra/level1: mapped the 24l bucket onto the spare bucket

--- data/levels/extra/level1.pl
+++ data/levels/extra/level1.pl
@@ -2,14 +2,14 @@ $level = new Guckets::Level();
 $level->{author} = "Lars Stoltenow";
 $level->{name} = "Extra Level 1";
 
-push(@{$level->{buckets}}, new Guckets::Bucket(24, 24));
-push(@{$level->{buckets}}, new Guckets::Bucket(0, 5));
 push(@{$level->{buckets}}, new Guckets::Bucket(0, 11));
+push(@{$level->{buckets}}, new Guckets::Bucket(0, 5));
 push(@{$level->{buckets}}, new Guckets::Bucket(0, 13));
 
-push(@{$level->{goals}}, Guckets::Goals::water(2, 8));
+push(@{$level->{goals}}, Guckets::Goals::water(1, 8));
+push(@{$level->{goals}}, Guckets::Goals::water(2, 0));
 push(@{$level->{goals}}, Guckets::Goals::water(3, 8));
-push(@{$level->{goals}}, Guckets::Goals::water(4, 8));
+# implicit: spare bucket has 8
 
-$level->{spare_bucket}->{water} = 0;
-$level->{spare_bucket}->{water_max} = 0;
+$level->{spare_bucket}->{water} = 24;
+$level->{spare_bucket}->{water_max} = 24;
