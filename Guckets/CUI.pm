e1b947ba5631935a8fbf50f85fbf08833e335375
Lars Stoltenow <penma@penma.de>
2009-06-07 18:47:38 +0200

    reintroduce the CUI
    Because it works.
    
    This reverts commit 3bd9e9cc5ae201d625af42e961f09079204e2b19.

---
+++ Guckets/CUI.pm
@@ -0,0 +1,15 @@
+package Guckets::CUI;
+
+use strict;
+use warnings;
+use 5.010;
+use Text::Autoformat;
+use myterm;
+
+use Guckets::CUI::Primitives;
+use Guckets::CUI::Dialog;
+use Guckets::CUI::Render;
+use Guckets::CUI::PlayLevel;
+use Guckets::CUI::PlayLevelset;
+
+1;
