591dddc9c10434837753f04d7093848a3dc4cdb1
Lars Stoltenow <penma@penma.de>
2009-06-10 16:09:37 +0200

    fix naming for levels

--- data/levels/default.pl
+++ data/levels/default.pl
@@ -2,4 +2,4 @@ $levelset = new Guckets::Levelset();
 $levelset->{author} = "Various";
 $levelset->{name} = "Default Levelset";
 
-push(@{$levelset->{levels}}, "default/level$_.pl") for (1..12);
+push(@{$levelset->{levels}}, "default/$_") for (1..12);
