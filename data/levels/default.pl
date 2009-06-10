e9c246d1e234e170133fe9a52b75b2157cc7a9da
Lars Stoltenow <penma@penma.de>
2009-06-10 16:33:33 +0200

    levels: update default.pl to add recently added level 13

--- data/levels/default.pl
+++ data/levels/default.pl
@@ -2,4 +2,4 @@ $levelset = new Guckets::Levelset();
 $levelset->{author} = "Various";
 $levelset->{name} = "Default Levelset";
 
-push(@{$levelset->{levels}}, "default/$_") for (1..12);
+push(@{$levelset->{levels}}, "default/$_") for (1..13);
