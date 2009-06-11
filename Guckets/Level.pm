de7985d3c716595833788babf67945c429d9f28e
Lars Stoltenow <penma@penma.de>
2009-06-11 16:56:07 +0200

    backend: install some primitive replay recording mechanism
    will record to GUCKETS_REPLAY_OUT if set.

--- Guckets/Level.pm
+++ Guckets/Level.pm
@@ -2,6 +2,9 @@ package Guckets::Level;
 
 use strict;
 use warnings;
+use 5.010;
+
+use Time::HiRes qw(time);
 
 use Guckets::Bucket;
 use Guckets::Goals;
@@ -40,31 +43,46 @@ sub auto_spare_bucket {
 # (which are really just the same as pouring from/to the spare bucket).
 sub bucket_fill {
 	my ($self, $bucket) = @_;
+	rp_save($self, "fill $bucket");
 	if ($self->{hooks}->{fill}) {
 		my $r = $self->{hooks}->{fill}->($self, $bucket);
-		return $r if (defined($r));
+		if (defined($r)) {
+			rp_save($self, "hook executed, replied: $r");
+			return $r;
+		}
 	}
 	$self->{spare_bucket}->pour_to($self->{buckets}->[$bucket]);
+	rp_save($self, "fill completed");
 	return undef;
 }
 
 sub bucket_empty {
 	my ($self, $bucket) = @_;
+	rp_save($self, "empty $bucket");
 	if ($self->{hooks}->{empty}) {
 		my $r = $self->{hooks}->{empty}->($self, $bucket);
-		return $r if (defined($r));
+		if (defined($r)) {
+			rp_save($self, "hook executed, replied: $r");
+			return $r;
+		}
 	}
 	$self->{buckets}->[$bucket]->pour_to($self->{spare_bucket});
+	rp_save($self, "empty completed");
 	return undef;
 }
 
 sub bucket_pour {
 	my ($self, $bucket1, $bucket2) = @_;
+	rp_save($self, "pour $bucket1 -> $bucket2");
 	if ($self->{hooks}->{pour}) {
 		my $r = $self->{hooks}->{pour}->($self, $bucket1, $bucket2);
-		return $r if (defined($r));
+		if (defined($r)) {
+			rp_save($self, "hook executed, replied: $r");
+			return $r;
+		}
 	}
 	$self->{buckets}->[$bucket1]->pour_to($self->{buckets}->[$bucket2]);
+	rp_save($self, "pour completed");
 	return undef;
 }
 
@@ -79,7 +97,38 @@ sub goal_check {
 		return 0 unless $_->{callback}->($self);
 	}
 
+	rp_save($self, "goals reached");
+
 	return 1;
+}
+
+# log an action to the reply file
+sub rp_save {
+	my ($level, $action) = @_;
+	return if (!defined($ENV{GUCKETS_REPLAY_OUT}));
+
+	# dump the buckets
+	my $buckets = sprintf("[%s]%d/%d", "spare",
+		$level->{spare_bucket}->{water},
+		$level->{spare_bucket}->{water_max},
+	);
+	for (my $i = 0; $i < scalar(@{$level->{buckets}}); $i++) {
+		$buckets .= sprintf("[%d]%d/%d", $i,
+			$level->{buckets}->[$i]->{water},
+			$level->{buckets}->[$i]->{water_max},
+		);
+	}
+
+	open(FH, ">>", $ENV{GUCKETS_REPLAY_OUT}) or return;
+	print FH sprintf(
+		"session=%s, time=%f, title=%d:%s, buckets=%s, action=%s\n",
+		$ENV{GUCKETS_REPLAY_INFO} // "(pid $$)",
+		time(),
+		length($level->{name}), $level->{name},
+		$buckets,
+		$action
+	);
+	close(FH);
 }
 
 1;
