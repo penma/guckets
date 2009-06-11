1fc5401522ca4efacdc1e1bafe62553a30ac5e24
Lars Stoltenow <penma@penma.de>
2009-06-11 14:07:48 +0200

    levels: add level 15

--- data/levels/default.pl
+++ data/levels/default.pl
@@ -2,4 +2,4 @@ $levelset = new Guckets::Levelset();
 $levelset->{author} = "Various";
 $levelset->{name} = "Default Levelset";
 
-push(@{$levelset->{levels}}, "default/$_") for (1..14);
+push(@{$levelset->{levels}}, "default/$_") for (1..15);
